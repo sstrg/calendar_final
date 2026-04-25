dcd0cdc8c8031f4cfbc1a4e0ae0b44b963601245
sstrg <164503103+sstrg@users.noreply.github.com>
2026-04-25 12:34:22 +0300

    Update CalendarViewModel.swift

--- Calendar/Calendar/CalendarViewModel.swift
+++ Calendar/Calendar/CalendarViewModel.swift
@@ -20,7 +20,6 @@ final class CalendarViewModel: ObservableObject {
         
         isLoading = true
         
-        // Просто загружаем локальные события
         loadLocalEvents(for: userId)
         
         isLoading = false
